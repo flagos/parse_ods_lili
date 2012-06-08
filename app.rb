173151c3f7f31c9f94d4f5f22d9f0242b3b09e54
flagos <flagospub@gmail.com>
2012-06-08 23:26:16 +0200

    parse all sheets

--- app.rb
+++ app.rb
@@ -4,27 +4,29 @@ require "roo"
 
 
 oo = Openoffice.new("fid.ods")
-oo.default_sheet = oo.sheets.first
-2.upto(29) do |line|
-  name        = oo.cell(line,'A')
-  birthday    = oo.cell(line,'B')
-  mail        = oo.cell(line,'C')
-  panier = Array.new
+oo.sheets.each do |sheet|
+  oo.default_sheet = sheet
+  2.upto(29) do |line|
+    name        = oo.cell(line,'A')
+    birthday    = oo.cell(line,'B')
+    mail        = oo.cell(line,'C')
+    panier = Array.new
 
-  panier_idx = ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
-  panier_idx.each do |idx|
-    one_buy = oo.cell(line, idx)
-    if (!one_buy.nil?)
-      puts "#{one_buy} @#{idx}"
-      date, price = one_buy.split
-      panier << { "date"  => date,
-                  "price" => price}
+    panier_idx = ["E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+    panier_idx.each do |idx|
+      one_buy = oo.cell(line, idx)
+      if (!one_buy.nil?)
+        puts "#{one_buy} @#{idx}"
+        date, price = one_buy.split
+        panier << { "date"  => date,
+          "price" => price}
+      end
+    end
+    if name
+      puts name
+      puts birthday
+      puts mail
+      puts panier.inspect
     end
   end
- if name
-   puts name
-   puts birthday
-   puts mail
-   puts panier.inspect
- end
 end
